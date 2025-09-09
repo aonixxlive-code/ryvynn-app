b54fb5f9dadc105c67f563a1dad8bded5f4bd35d
aonixxlive-code <aonixx.live@gmail.com>
2025-09-08 20:28:14 -0700

    chore: add success page

---
+++ pages/success.js
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
+      <h1>Payment Successful</h1>
+      <p>Thank you for subscribing to RYVYNN Plus!</p>
+    </div>
+  );
+}
